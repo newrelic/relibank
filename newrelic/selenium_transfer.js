e9a5b08e68f34486b6729ff4f117b7de67a677bf
jgoddard <jgoddard@newrelic.com>
2025-12-12 09:04:58 -0800

    another scenario

--- newrelic/selenium_transfer.js
+++ newrelic/selenium_transfer.js
@@ -87,6 +87,111 @@ $browser.get('http://relibank.westus2.cloudapp.azure.com/').then(function(){
                                             return alertMessage.getText().then(function(text){
                                               assert.ok(text.includes('Successfully transferred'), 'Success message should confirm transfer');
                                               console.log('Transfer successful! Message: ' + text);
+
+                                              // Wait a moment before clicking other buttons
+                                              return $browser.sleep(1000).then(function(){
+
+                                                // Click the "Show All" button in transactions if it exists
+                                                return $browser.findElements($driver.By.xpath("//button[contains(text(), 'Show All')]")).then(function(showAllButtons){
+                                                  if (showAllButtons.length > 0) {
+                                                    console.log('Found Show All transactions button');
+                                                    return showAllButtons[0].click().then(function(){
+                                                      console.log('Clicked Show All transactions button');
+                                                      return $browser.sleep(500);
+                                                    });
+                                                  } else {
+                                                    console.log('Show All button not found, skipping');
+                                                    return Promise.resolve();
+                                                  }
+                                                }).then(function(){
+
+                                                  // Click the "Show Less" button if it exists
+                                                  return $browser.findElements($driver.By.xpath("//button[contains(text(), 'Show Less')]")).then(function(showLessButtons){
+                                                    if (showLessButtons.length > 0) {
+                                                      console.log('Found Show Less transactions button');
+                                                      return showLessButtons[0].click().then(function(){
+                                                        console.log('Clicked Show Less transactions button');
+                                                        return $browser.sleep(500);
+                                                      });
+                                                    } else {
+                                                      console.log('Show Less button not found, skipping');
+                                                      return Promise.resolve();
+                                                    }
+                                                  }).then(function(){
+
+                                                    // Now transfer the funds back to checking
+                                                    console.log('Starting reverse transfer back to checking');
+
+                                                    // Scroll back to the transfer form
+                                                    return $browser.findElement($driver.By.css('input[type="number"]')).then(function(amountField){
+                                                      console.log('Found amount field for reverse transfer');
+
+                                                      // Clear and enter the same amount
+                                                      return amountField.clear().then(function(){
+                                                        return amountField.sendKeys('100').then(function(){
+                                                          console.log('Entered reverse transfer amount: $100');
+
+                                                          // Find the "From" dropdown and select savings
+                                                          return $browser.findElement($driver.By.id('from-account-select')).then(function(fromSelect){
+                                                            console.log('Found from account dropdown for reverse transfer');
+
+                                                            return fromSelect.click().then(function(){
+                                                              console.log('Opened from account dropdown');
+
+                                                              return $browser.sleep(500).then(function(){
+
+                                                                // Select savings as from account
+                                                                return $browser.findElement($driver.By.css('li[data-value="savings"]')).then(function(savingsOption){
+                                                                  return savingsOption.click().then(function(){
+                                                                    console.log('Selected savings as from account');
+
+                                                                    // Wait for the dropdown to close before clicking the next one
+                                                                    return $browser.sleep(1000).then(function(){
+
+                                                                      // Find the "To" dropdown and select checking
+                                                                      return $browser.findElement($driver.By.id('to-account-select')).then(function(toSelect){
+                                                                        console.log('Found to account dropdown');
+
+                                                                        return toSelect.click().then(function(){
+                                                                          console.log('Opened to account dropdown');
+
+                                                                          return $browser.sleep(500).then(function(){
+
+                                                                            // Select checking as to account
+                                                                            return $browser.findElement($driver.By.css('li[data-value="checking"]')).then(function(checkingOption){
+                                                                              return checkingOption.click().then(function(){
+                                                                                console.log('Selected checking as to account');
+
+                                                                                // Click the Complete Transfer button again
+                                                                                return $browser.findElement($driver.By.css('button[type="submit"]')).then(function(transferButton){
+                                                                                  console.log('Found Complete Transfer button for reverse transfer');
+
+                                                                                  return transferButton.click().then(function(){
+                                                                                    console.log('Clicked Complete Transfer button for reverse transfer');
+
+                                                                                    // Wait for success message
+                                                                                    return $browser.sleep(2000).then(function(){
+                                                                                      console.log('Reverse transfer completed');
+                                                                                    });
+                                                                                  });
+                                                                                });
+                                                                              });
+                                                                            });
+                                                                          });
+                                                                        });
+                                                                      });
+                                                                    });
+                                                                  });
+                                                                });
+                                                              });
+                                                            });
+                                                          });
+                                                        });
+                                                      });
+                                                    });
+                                                  });
+                                                });
+                                              });
                                             });
                                           });
                                         });
